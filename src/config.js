2316df80e0983164dba911cf543a3e9e7c9fddb8
chaudharyarpan79-cyber <chaudharyarpan79@gmail.com>
2026-04-24 22:04:29 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  GOOGLE_SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsZiM5Xyy7VLBfeG9EFt9HDS7JDBwBi2indIg3mfZuqokDrQeq5OIZQftzCZavfQ/pub?output=csv",
+  TITLE: "POD Timeline Dashboard",
+  FY_LABEL: "FY25-26",
+  REFRESH_MINUTES: 5,
+};
+
+export default CONFIG;
